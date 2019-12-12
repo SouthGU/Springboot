f02e6133ab74fd7d47823ab18cff3b9ed96c7b21
SouthGU <2370015271@qq.com>
2019-12-12 16:00:29 +0800

    微信支付demo --> 2019/12/12.

--- Springboot/src/main/java/SpringbootDemo/ComprehensiveTraining/DepartRepository.java
+++ Springboot/src/main/java/SpringbootDemo/ComprehensiveTraining/DepartRepository.java
@@ -2,6 +2,8 @@ package SpringbootDemo.ComprehensiveTraining;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
 
 public interface DepartRepository extends JpaRepository<Depart,Integer> {
+
 }
